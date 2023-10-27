b9a7eb9c2a6987a4a0c7a2e1ed555efd29e8df13
SayedAdnanDev <g3agletech@gmail.com>
2023-10-27 16:04:30 +0300

    added a singleton class for the database

---
+++ DB.php
@@ -0,0 +1,59 @@
+<?php
+class Database extends MySQLi
+{
+    /**
+     * Singleton instance of MySQLi
+     *
+     * @var self
+     */
+    private static $instance = null;
+
+    private function __construct($host, $user, $password, $database)
+    {
+        parent::__construct($host, $user, $password, $database);
+    }
+
+    /**
+     * Instance of MySQLi
+     *
+     * @return self
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self("localhost", "root", "420pOg9118169", "Employees");
+        }
+
+        return self::$instance;
+    }
+
+
+    /**
+     * Prepare and execute statements and bind passed types and values.
+     *
+     * @param string $query
+     * @param string $types
+     * @param array $values
+     * @return mysqli_stmt|false
+     */
+    public static function executeStatement($query, $types = '', array $values = [])
+    {
+        $db = static::getInstance();
+        $stmt = $db->prepare($query);
+
+        if (strlen($types) > 0 && count($values) > 0) {
+            $stmt->bind_param($types, ...$values);
+        }
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function closeConnection()
+    {
+        if (self::$instance !== null) {
+            parent::close();
+            self::$instance = null;
+        }
+    }
+}
